67e9e7066a9af74b1629580473e3cc153e4f9d8e
JoelDamian <joel.damian.echeverria@gmail.com>
2022-04-28 18:42:13 -0400

    ISP-38 Added react router dom package and configure routes.

--- SpecialOneWeb/src/App.js
+++ SpecialOneWeb/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-     Hello World!
-    </div>
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<div>Hello World</div>} />
+                <Route path="/edit-personal-info" element={<div>Person Info</div>} />
+                <Route path="/edit-contact-info" element={<div>Contact Info</div>} />
+            </Routes>
+        </div>
   );
 }
 
