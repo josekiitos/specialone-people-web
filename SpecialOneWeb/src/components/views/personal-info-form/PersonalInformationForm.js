9673ddf5f9797fdbaecbe5f46076e6c271a87c84
JoelDamian <joel.damian.echeverria@gmail.com>
2022-04-29 19:25:49 -0400

    ISP-40 Added logic to change the textFields.

--- SpecialOneWeb/src/components/views/personal-info-form/PersonalInformationForm.js
+++ SpecialOneWeb/src/components/views/personal-info-form/PersonalInformationForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Avatar from '@mui/material/Avatar';
@@ -19,8 +19,25 @@ const PersonalInformationForm = () => {
         } catch (error) {
             console.error('Error retreiving personal information' + error)
         }
-        console.log(personInfo.data)
         setPersonalInfo(personInfo.data);
+    }
+
+    const handleChangeFirstName = (event) => {
+        let newPersonalInfo = { ...personalInfo };
+        newPersonalInfo.firstName = event.target.value;
+        setPersonalInfo(newPersonalInfo);
+    }
+
+    const handleChangeLastName = (event) => {
+        let newPersonalInfo = { ...personalInfo };
+        newPersonalInfo.lastName = event.target.value;
+        setPersonalInfo(newPersonalInfo);
+    }
+
+    const handleChangeNickName = (event) => {
+        let newPersonalInfo = { ...personalInfo };
+        newPersonalInfo.nickName = event.target.value;
+        setPersonalInfo(newPersonalInfo);
     }
 
     return (
@@ -42,14 +59,16 @@ const PersonalInformationForm = () => {
                 }}
             />
             <Box sx={{ width: '100%', marginLeft: '25%'}}>
-                <TextField id="outlined-basic" label="First Name" variant="outlined"
-                    value={personalInfo.firstName}
+                <TextField id="firstName" label="First Name" variant="outlined"
+                    value={personalInfo.firstName} onChange={handleChangeFirstName}
                     fullWidth style={{ marginTop: 15, width: '60%' }} />
-                <TextField id="outlined-basic" label="Last Name" variant="outlined"
+                <TextField id="lastName" label="Last Name" variant="outlined"
                     value={personalInfo.lastName}
+                    onChange={handleChangeLastName}
                     fullWidth style={{ marginTop: 10, width: '60%' }} />
-                <TextField id="outlined-basic" label="Nick Name" variant="outlined"
+                <TextField id="nickName" label="Nick Name" variant="outlined"
                     value={personalInfo.nickName}
+                    onChange={handleChangeNickName}
                     fullWidth style={{ marginTop: 10, width: '60%' }} />
             </Box>
             <Button variant="contained" style={{ marginTop: 15, marginLeft: '50%' }}>Submit</Button>
